8824b76ecd352d64430f90f29312a6251bb8ad5a
Nancy McKelvey <nancymckelvey@Nancys-iMac.lan>
2022-11-11 22:01:49 +1000

    finishing first proj.

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
 import datetime
-
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -14,7 +13,14 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -22,4 +28,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-
